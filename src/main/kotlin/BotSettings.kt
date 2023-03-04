2720aa58a1623b92ab8ce4d4cabaf8ceccbc2241
neetkee <neetkee@icloud.com>
2023-03-04 21:44:14 +0600

    Refine the reading of OPENAI_KEY and use provided message roles.

--- src/main/kotlin/BotSettings.kt
+++ src/main/kotlin/BotSettings.kt
@@ -3,4 +3,5 @@ object BotSettings {
     val telegramBotUsername = EnvReader.getEnv("TELEGRAM_BOT_USERNAME")
     val telegramBotAdminId = EnvReader.getEnv("TELEGRAM_BOT_ADMIN_ID").toLong()
     val sqlitePath = EnvReader.getEnv("SQLITE_PATH")
+    val openAIKey = EnvReader.getEnv("OPENAI_KEY")
 }
--- src/main/kotlin/Gpt.kt
+++ src/main/kotlin/Gpt.kt
@@ -7,7 +7,7 @@ import com.aallam.openai.client.OpenAI
 
 @OptIn(BetaOpenAI::class)
 object Gpt {
-    private val openAI = OpenAI(EnvReader.getEnv("OPENAI_KEY"))
+    private val openAI = OpenAI(BotSettings.openAIKey)
     private val userContexts = mutableMapOf<Long, MutableList<ChatMessage>>()
 
     fun clearUserContext(userId: Long) {
@@ -18,19 +18,19 @@ object Gpt {
 
     suspend fun getResponse(userId: Long, message: String): Result<String> = runCatching {
         val chatCompletionMessage = ChatMessage(
-            role = ChatRole("user"),
+            role = ChatRole.User,
             content = message
         )
         userContexts.getOrPut(userId) { mutableListOf() }.add(chatCompletionMessage)
 
         val chatCompletionRequest = ChatCompletionRequest(
-            model = ModelId("gpt-3.5-turbo-0301"),
+            model = ModelId("gpt-3.5-turbo"),
             messages = userContexts.getValue(userId)
         )
         val responseContent = openAI.chatCompletion(chatCompletionRequest).choices.first().message!!.content
         userContexts.getValue(userId).add(
             ChatMessage(
-                role = ChatRole("assistant"),
+                role = ChatRole.Assistant,
                 content = responseContent
             )
         )
