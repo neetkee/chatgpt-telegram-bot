fb9af3e395e29bbf09a893b90e0fcb079ed87661
neetkee <neetkee@icloud.com>
2023-04-22 09:19:24 +0300

    Increase OpenAI socket timeout value.

--- src/main/kotlin/Gpt.kt
+++ src/main/kotlin/Gpt.kt
@@ -2,18 +2,21 @@ import com.aallam.openai.api.BetaOpenAI
 import com.aallam.openai.api.chat.ChatCompletionRequest
 import com.aallam.openai.api.chat.ChatMessage
 import com.aallam.openai.api.chat.ChatRole
+import com.aallam.openai.api.http.Timeout
 import com.aallam.openai.api.logging.LogLevel
 import com.aallam.openai.api.logging.Logger
 import com.aallam.openai.api.model.ModelId
 import com.aallam.openai.client.OpenAI
 import com.aallam.openai.client.OpenAIConfig
+import kotlin.time.Duration.Companion.minutes
 
 @OptIn(BetaOpenAI::class)
 object Gpt {
     private val openAIConfig = OpenAIConfig(
         token = BotSettings.openAIKey,
         logger = Logger.Default,
-        logLevel = LogLevel.None
+        logLevel = LogLevel.None,
+        timeout = Timeout(socket = 2.minutes)
     )
     private val openAI = OpenAI(openAIConfig)
     private val userContexts = mutableMapOf<Long, MutableList<ChatMessage>>()
