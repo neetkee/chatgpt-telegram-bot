d339e162835c780b1bdac1e54680be16db9afbbb
Artem Yakshin <neetkee@icloud.com>
2024-01-20 09:57:34 +0300

    Update deps.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.22"
     id("com.google.cloud.tools.jib") version "3.4.0"
     id("com.github.ben-manes.versions") version "0.50.0"
     application
@@ -19,11 +19,11 @@ dependencies {
     implementation("org.telegram:telegrambots:6.8.0")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
     implementation("ch.qos.logback:logback-classic:1.4.14")
-    implementation("org.xerial:sqlite-jdbc:3.44.1.0")
+    implementation("org.xerial:sqlite-jdbc:3.45.0.0")
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    implementation(platform("com.aallam.openai:openai-client-bom:3.6.1"))
+    implementation(platform("com.aallam.openai:openai-client-bom:3.6.3"))
     implementation("com.aallam.openai:openai-client")
     implementation("io.ktor:ktor-client-cio")
 
